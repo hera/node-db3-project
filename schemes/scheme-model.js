a14f2b9031fad4610b8cc42aaab61afa1a1724f4
hera <a.heraimenka@gmail.com>
2020-07-15 15:02:28 -0700

    Add scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,8 @@ const db = require("../data/dbConfig");
 module.exports = {
     find,
     findById,
-    findSteps
+    findSteps,
+    add
 };
 
 function find () {
@@ -19,4 +20,12 @@ function findSteps (id) {
         .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
         .join("steps", "schemes.id", "=", "steps.scheme_id")
         .where({"schemes.id": id});
+}
+
+function add (scheme) {
+    return db("schemes")
+        .insert(scheme, "id")
+        .then(ids => {
+            return findById(ids[0]);
+        })
 }
